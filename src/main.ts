dc90da6844b7df7ba3af7533b68b38bdcb206e10
Alexandre Fukano <alexandremkimura@hotmail.com>
2023-05-23 23:44:27 -0300

    creating user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Somente as infos que precisar nos controllers
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
