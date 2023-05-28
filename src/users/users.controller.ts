d26bf986a042a2c1ab83bd8f325f4a0e15376f34
Alexandre Fukano <alexandremkimura@hotmail.com>
2023-05-28 18:03:15 -0300

    implements interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,22 @@
 import {
   Body,
   Controller,
-  Post,
-  Get,
-  Patch,
   Delete,
-  Param,
-  Query,
+  Get,
   NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) { }
 
@@ -22,6 +25,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  //Utilizado juntamente com o Exclude Decorator na Entity | Refatorado depois para o Serialize Decorator criado
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const userExists = await this.usersService.findOne(Number(id));
