5f5d642fa5956f95b7563cf158c8b62a62cf07c9
Alexandre Fukano <alexandremkimura@hotmail.com>
2023-06-20 23:43:39 -0300

    integration tests

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asvafasd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      //Somente as infos que precisar nos controllers
-      whitelist: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
